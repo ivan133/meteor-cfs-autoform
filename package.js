1ecfb4f786de5e96064ff3c9e16c9b8664ff428a
Ivan <ivan133133@gmail.com>
2018-10-19 10:23:42 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "cfs:autoform",
-  version: "2.2.1",
+  version: "2.2.2",
   summary: "Upload files as part of autoform submission",
   git: "https://github.com/aldeed/meteor-cfs-autoform.git"
 });
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('underscore@1.0.1', 'client');
   api.use('templating@1.0.9', 'client');
 
-  api.use('aldeed:autoform@4.0.0 || 5.0.0');
+  api.use('aldeed:autoform@4.0.0 || 5.0.0 || 6.0.0');
   api.use('cfs:standard-packages@0.0.2', ['client', 'server'], {weak: true});
   api.use('raix:ui-dropped-event@0.0.7', 'client');
 
